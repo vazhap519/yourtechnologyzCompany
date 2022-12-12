3e84b42275ebd6feabbcb61c7a386309af270bf9
vazha <phankvelashvili.mail@gmail.com>
2022-12-12 16:45:23 +0400

    add homeslider component

---
+++ app/Models/HomesliderModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HomesliderModel extends Model
+{
+    use HasFactory;
+}
